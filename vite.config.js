9c3544b6fac875195778083075fa4ff3849e8431
Olakareem <olakareemomobolarinwa@gmail.com>
2024-10-25 07:34:41 +0100

    fixes

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,3 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-
-//   server: {
-//     port: 3000,
-//     proxy: {
-//       "/api": {
-//         target: "http://localhost:8800",
-//         changeOrigin: true,
-//       },
-//     },
-//   },
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -27,9 +9,27 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "https://server-by6klkyl4-olaspankys-projects.vercel.app", // Change this linessrfgs
+        target: "http://localhost:8800",
         changeOrigin: true,
       },
     },
   },
 });
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+
+//   server: {
+//     port: 3000,
+//     proxy: {
+//       "/api": {
+//         target: "https://server-by6klkyl4-olaspankys-projects.vercel.app", // Change this linessrfgs
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// });
