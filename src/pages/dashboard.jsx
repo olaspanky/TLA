133d55091a5b6ac0b590bfdc87ff9e8f70c6ce50
Olakareem <olakareemomobolarinwa@gmail.com>
2024-12-03 14:19:42 +0100

    fix

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -126,10 +126,10 @@ const UserTable = ({ users }) => {
         <p
           className={clsx(
             "w-fit px-3 py-1 rounded-full text-sm",
-            user?.isActive ? "bg-blue-200" : "bg-yellow-100"
+            user?.isActive ? "bg-blue-200" : "bg-blue-200"
           )}
         >
-          {user?.isActive ? "Active" : "Disabled"}
+          {user?.isActive ? "Active" : "Active"}
         </p>
       </td>
       <td className='py-2 text-sm'>{moment(user?.createdAt).fromNow()}</td>
