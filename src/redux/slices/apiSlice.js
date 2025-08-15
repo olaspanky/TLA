306e51bbe1fd4f5b4c26d35ce566f919b88a4256
adegoke ayobami <adegokeayobamiseun@gmail.com>
2025-08-15 10:41:03 +0100

    fix:reporting to issues == sol:current user not initialized

--- src/redux/slices/apiSlice.js
+++ src/redux/slices/apiSlice.js
@@ -2,18 +2,18 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
-  reducerPath: 'api',
+  reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://traffic.pbr.com.ng/api',
+    baseUrl: "http://localhost:8800/api",
     prepareHeaders: (headers, { getState }) => {
-      const token = getState().auth.token || localStorage.getItem('authToken');
+      const token = getState().auth.token || localStorage.getItem("authToken");
       if (token) {
-        headers.set('Authorization', `Bearer ${token}`);
+        headers.set("Authorization", `Bearer ${token}`);
       }
-      headers.set('Content-Type', 'application/json');
+      headers.set("Content-Type", "application/json");
       return headers;
     },
   }),
-  tagTypes: ['Objective', 'Task', 'User', 'Department', 'Organization'], // Add new tags
+  tagTypes: ["Objective", "Task", "User", "Department", "Organization"],
   endpoints: () => ({}),
 });
